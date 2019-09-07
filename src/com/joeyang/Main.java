015a9b4edb1eaabe255e9e29fb12128143190eac
Zoe Young <yangzhou1989@gmail.com>
2019-09-07 12:05:49 +0800

    add problem 226

--- src/com/joeyang/Main.java
+++ src/com/joeyang/Main.java
@@ -3,6 +3,8 @@ package com.joeyang;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Problem1108 problem1108 = new Problem1108();
+        String s = problem1108.defangIPaddr("1.1.1.1");
+        System.out.println(s);
     }
 }
---
+++ src/com/joeyang/Problem942.java
@@ -0,0 +1,13 @@
+package com.joeyang;
+
+public class Problem942 {
+    public int[] diStringMatch(String S) {
+        int length = S.length();
+        for (int i1 = 0; i1 <= length; i1++) {
+            for (int i2 = 0; i2 <= length; i2++) {
+
+            }
+        }
+        return null;
+    }
+}
